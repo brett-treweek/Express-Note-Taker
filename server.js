390a0f67d600f1dc861c7d6370ae512a1ebb6970
Brett Treweek <bretttrew@gmail.com>
2021-05-15 16:35:46 +0800

    added id to new notes

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const id = require('nanoid');
 const fs = require('fs');
 const app = express();
 const port = 3000;
 
 
-// Setting static folder so css and js works
+// Setting static folder so css and js works----------------------
 app.use(express.static('./Develop/public'));
 app.use(express.json());
 
+// HTML routes set-------------------------------------------------
 app.get('/', (req, res) => {
     res.sendFile('./Develop/public/index.html', { root: __dirname})
 });
@@ -16,40 +18,42 @@ app.get('/notes', (req, res) => {
     res.sendFile('./Develop/public/notes.html', { root: __dirname})
 });
 
+// API routes set-------------------------------------------------
 app.get('/api/notes', (req, res) => {
     res.sendFile('./Develop/db/db.json', { root: __dirname})
 });
 
-app.post('/api/notes', (req, res) => {
-    console.log('i got a request');
-    console.log(req.body);
 
-    
-    const json = (req.body);
-    
-    fs.readFile("./Develop/db/db.json", (err, data) => {
+app.post("/api/notes", (req, res) => {
+  console.log("i got a request");
+  console.log(req.body);
+
+  const json = req.body;
+  req.body.id = id.nanoid();
+
+  fs.readFile("./Develop/db/db.json", (err, data) => {
+    if (err) throw err;
+    let jsonArray = JSON.parse(data);
+    jsonArray.push(json);
+
+    fs.writeFile(
+      "./Develop/db/db.json",
+      JSON.stringify(jsonArray),
+      function (err) {
         if (err) throw err;
-        let jsonArray = JSON.parse(data);
-        jsonArray.push(json);
-
-        fs.writeFile("./Develop/db/db.json", JSON.stringify(jsonArray) , function (err) {
-            if (err) throw err;
-            console.log('The new Note was appended to db.json!');
-         });
-      });
-
-    
-
-    res.json({
-        status: 'success'
-    },
-    
-)})
+        console.log("The new Note was appended to db.json!");
+      }
+    );
+  });
+  res.json({
+    status: "success",
+  });
+});
 
 
 
 
-// Listen for requests
+// Listen for requests----------------------------------------
 app.listen(port, () => {
     console.log(`app listening on port:${port}`)
 })
